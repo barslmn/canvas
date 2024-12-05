b9ac2be9fe94a321332222d340e491e824e99324
handebeklen <handebeklenn@gmail.com>
2024-12-05 19:37:57 +0300

    added version

--- canvas/views.py
+++ canvas/views.py
@@ -1130,7 +1130,7 @@ def create_report(request):
     chip_id = chipsample.chip.chip_id
     chip_type = chipsample.chip.chip_type.name
     version = get_version()
-    
+
     if not settings.DEBUG:
         HOST_IP = get_default_gateway_linux()
         MINIO_IP = socket.gethostbyname("minio")
@@ -1174,7 +1174,7 @@ def create_report(request):
                                                 --cnvs {cnv_file.name} \
                                                 --institute "\\"{chipsample.sample.institution.name}\\"" \
                                                 --protocol_id "\\"{chipsample.sample.protocol_id}\\"" \
-                                                --version {version}
+                                                --version {version} \
                                                 -c {nfc.name} \
                                                 -with-report {chip_id}_{label}.html \
                                                 -profile docker',
