3ca3c9d3656cc8039eff1357ff648c1e9fccf187
Barış Salman <barslmn@gmail.com>
2025-01-09 01:35:40 +0300

    build change

--- canvas/models.py
+++ canvas/models.py
@@ -1,5 +1,4 @@
 from django.core.validators import RegexValidator, FileExtensionValidator
-from django.core.exceptions import ValidationError
 
 from django.db import models
 from django.contrib.auth.models import User, Group
